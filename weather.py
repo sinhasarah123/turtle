3fd0925659023814afdf0db34e8add6faf92897d
sarahsinha123 <sinhasarah47@gmail.com>
2025-08-07 16:23:23 +0530

    weather conditions

---
+++ weather.py
@@ -0,0 +1,7 @@
+def weather_condition():
+    spring="autumn"
+    autumn="spring"
+
+    print("The weather is pleasant in", autumn)
+    print("the weather is same in spring")
+weather_condition()
