fb0a8d56e2f2e0123c1077d6ea407fb857df2cb3
sarahsinha123 <sinhasarah47@gmail.com>
2025-08-11 18:54:32 +0530

    waiter

--- tip,thewaiter.py
+++ tip,thewaiter.py
@@ -1,4 +1,5 @@
 def total_calculation(bill_amount, tip_percentage):
-    tip_amount = bill_amount * (tip_percentage / 100)
-    total_amount = bill_amount + tip_amount
-    return total_amount
+    total= bill_amount + (bill_amount * tip_percentage / 100)
+    total=round(total, 2)
+    return total
+print("please pay:", total_calculation(150, 20))
