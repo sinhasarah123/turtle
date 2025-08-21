b7b6619d393c3ddb46e5e8fcf1ff794267cf477e
sarahsinha123 <sinhasarah47@gmail.com>
2025-08-21 16:51:15 +0530

    maths

---
+++ math.py
@@ -0,0 +1,7 @@
+import math
+print("the ceiling of 23.56 is", math.ceil(23.56), "and the floor is", math.floor(23.56))
+print("the floor value of 23.56 is", math.floor(23.56))
+x=10
+y=-15
+print("the value of x after copying sign of y is", math.copysign(x, y))
+print("the absolute value of y is", abs(y))
