cc91a871e8d4d942c763d13bdfb7f481d29d253d
sarahsinha123 <sinhasarah47@gmail.com>
2025-08-25 18:45:40 +0530

    date and time checker

---
+++ date and time.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+today = datetime.today()
+now = datetime.now()
+print("Today's date:", today)
+print("Current time:", now)
+print("in date component:", today.day, today.month, today.year)
